671afe12144d1fab359049e9c3a8a1137bdc88ca
yuliiakanifolska <juliyakanifolskaya722@gmail.com>
2024-04-10 11:46:27 +0300

    Laba5

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace laba_5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть речення:");
+            string input = Console.ReadLine();
+
+            Sentence sentence = new Sentence(input);
+            sentence.GetTextInformation();
+            Console.WriteLine($"Кількість слів у реченні: {sentence.CountWords()}");
+            Console.WriteLine($"Алфавіт: {sentence.IdentifyAlphabet()}");
+
+            Console.WriteLine("\nВведіть ієрогліф:");
+            string hieroglyphInput = Console.ReadLine();
+
+            Hieroglyph hieroglyph = new Hieroglyph(hieroglyphInput);
+            hieroglyph.GetTextInformation();
+            
+            Console.WriteLine($"Походження ієрогліфа:");
+            hieroglyph.FindCJKCharacters();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Text.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace laba_5
+{
+    public abstract class Text 
+    {
+        public int symbolCount;
+        public string content;
+
+        public Text(string content)
+        {
+            this.content = content;
+            this.symbolCount = content.Length;
+        }
+
+        public virtual void GetTextInformation()
+        {
+            Console.WriteLine($"Кількість символів: {symbolCount}");
+        }
+
+        public string IdentifyAlphabet()
+        {
+            bool isCyrillic = false;
+            bool isLatin = false;
+
+            foreach (char c in content)
+            {
+                if ((c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я'))
+                {
+                    isCyrillic = true;
+                }
+                else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    isLatin = true;
+                }
+            }
+
+            if (isCyrillic)
+            {
+                return "Кирилиця";
+            }
+            if (isLatin)
+            {
+                return "Латиниця";
+            }
+            return
+                "Інший алфавіт";
+        }
+    }
+
+    public class Sentence : Text
+    {
+        public Sentence(string content) : base(content)
+        {
+        }
+
+        public int CountWords()
+        {
+            return content.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Count();
+        }
+
+        public override void GetTextInformation()
+        {
+            Console.WriteLine($"Речення: {content}");
+            base.GetTextInformation();
+        }
+
+
+    }
+
+    public class Word : Text
+    {
+        public Word(string content) : base(content)
+        {
+        }
+    }
+
+
+    public class Hieroglyph : Text
+    {
+        public Hieroglyph(string content) : base(content)
+        {
+        }
+
+
+        public void FindCJKCharacters()
+        {
+            
+            string[] unicodeRanges = { @"\p{IsCJKUnifiedIdeographs}", @"\p{IsHiragana}", @"\p{IsKatakana}", @"\p{IsHangulSyllables}" };
+            string[] languages = { "Китайський", "Японський (Хірагана)", "Японський (Катакана)", "Корейський" };
+
+            for (int i = 0; i < unicodeRanges.Length; i++)
+            {
+                MatchCollection matches = Regex.Matches(content, unicodeRanges[i]);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine($"Ієрогліф: {match.Value}, Мова: {languages[i]}");
+                }
+            }
+        }
+    }
+
+    public class Picture 
+    {
+        public Picture(string content) 
+        {
+        }
+    }
+
+}
+
